13d4a7ec700cbc7cfa164ffc302a65d08bcf94b1
rjmorla <royjmorla@gmail.com>
2021-10-28 10:26:29 -0700

    added base classes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from './components/Home';
 import logo from './logo.svg';
 import './css/App.css';
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
     render() {
         return(
             <div>
-                <h1>Component</h1>
+                <h1>Home</h1>
             </div>
         )
     }
